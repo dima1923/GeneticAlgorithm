4d3c178d63a13e150050b5a8df0373e216d12adc
dmitry <dima1923@me.com>
2019-09-18 13:36:25 +0300

    add

--- Crossover.py
+++ Crossover.py
@@ -1,2 +1,34 @@
 class Crossover:
-    pass
+    def __init__(self):
+        pass
+
+    def density_crossover(self):
+        pass
+
+    def intermediate_crossover(self):
+        pass
+
+    def linear_crossover(self):
+        pass
+
+    def single_point_crossover(self):
+        pass
+
+    def double_point_crossover(self):
+        pass
+
+    def multipoint_crossover(self):
+        pass
+
+    def uniform_crossover(self):
+        pass
+
+    def process_crossover(self):
+        pass
+
+    def shuffle_crossover(self):
+        pass
+
+    def replacement_crossover(self):
+        pass
+
