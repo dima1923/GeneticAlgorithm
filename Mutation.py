39b550e7abe88c750a8f057a53aa0de485fc347b
dmitry <dima1923@me.com>
2019-09-18 13:15:33 +0300

    add

---
+++ Mutation.py
@@ -0,0 +1,18 @@
+class Mutation:
+    def __init__(self):
+        pass
+
+    def double_mutation(self):
+        pass
+
+    def density_mutation(self):
+        pass
+
+    def real_mutation(self):
+        pass
+
+    def insertion_deleting_mutation(self):
+        pass
+
+    def changing_mutation(self):
+        pass
