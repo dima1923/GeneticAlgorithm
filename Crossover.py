b565e8c1a5fbeaa1d04c43866b4181227bd1cb57
ValLepskaya <50040376+ValLepskaya@users.noreply.github.com>
2019-10-17 14:31:50 +0300

    Update Crossover.py

--- Crossover.py
+++ Crossover.py
@@ -4,7 +4,7 @@ from random import randint
 import numpy as np
 
 class Crossover(Base):
-    def empty(self,parents):
+    def empty(self, parents):
         return parents
 
     def density_crossover(self):
@@ -56,43 +56,43 @@ class Crossover(Base):
 
     def replacement_crossover(self):
         pass
-
-    def crossover_NN(self, p1, p2, MainPos=2):
+    
+    def crossover_NN(self, p1, p2):
         """
         Хромосома ребенка наследует в вероятностью 50/50 ген одного из родителей.
         Если длина хромосомы будет наследована от большего родителя, то недостающие
         гены будут также унаследованы у этого родителя
         :param p1: хромосома 1 родителя
         :param p2: хромосома 2 родителя
-        :param MainPos: кол-во дополнительных признаков
         :return: хромосома наследника (наследников?)
         """
-        def error(p1, p2, pos):
+        MainPos=2
+        def error__NN(p1, p2, pos):
             if not (isinstance(p1, list) and isinstance(p2, list)):
                 raise ArithmeticError("one or both from parents not list")
             if (pos > len(p1) or pos > len(p2)):
                 raise ArithmeticError("this position %d out of range parent's lists")
-        def randBol(b=1):
+        def randBol_NN(b=1):
             return randint(0, b)
-        def introduction(p1, p2, pos):
+        def introduction_NN(p1, p2, pos):
             child = list()
             if pos != 0:
                 for i in range(0, pos):
-                    child.append(p2[i] if randBol() else p1[i])
+                    child.append(p2[i] if randBol_NN() else p1[i])
             return child
-        def adder(a, b):
-            child.append(a if randBol() else b)
+        def adder_NN(a, b):
+            child.append(a if randBol_NN() else b)
             return len(child)
 
         error(p1, p2, MainPos)
-        child = introduction(p1, p2, MainPos)
+        child = introduction_NN(p1, p2, MainPos)
         if (p2[MainPos] - p1[MainPos] < 0):
             p = p1.copy()
             p1 = p2.copy()
             p2 = p
-        pos = adder(p2[MainPos], p1[MainPos])
+        pos = adder_NN(p2[MainPos], p1[MainPos])
         for i in range(pos, p1[MainPos] + pos):
-            pos = adder(p2[i], p1[i])
+            pos = adder_NN(p2[i], p1[i])
         if child[MainPos] == p2[MainPos]:
             child += p2[pos:]
         return child
